8cb2b5e72e1d0f1ad884c8b10a2ba99369c94ae5
Kyle Graehl <kgraehl@gmail.com>
2015-10-01 15:00:00 -0700

    update package script

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 rm package.zip
 
-zip package.zip -r * -x package.sh -x *.git* -x "*.*~" -x cws_*.png -x *.scratch
+zip package.zip -r * -x package.sh -x *.git* -x "*.*~" -x images/cws_*.png -x *.scratch
 

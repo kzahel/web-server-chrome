49123a1aae761b19287afe4917d51652c6f54d71
PorygonZRocks <PorygonZRocks@users.noreply.github.com>
2017-07-20 09:49:55 -0500

    Update options.js

--- polymer-ui/options.js
+++ polymer-ui/options.js
@@ -30,6 +30,10 @@
             type: Boolean,
             default: false
         },
+        optCached: {
+        	type: Boolean,
+        	default: true
+        },
         optTryOtherPorts: {
             type: Boolean,
             default: false
@@ -58,57 +62,43 @@
         optUpload: {
             type: Boolean,
             default: false
-        },
-        optModRewriteEnable: {
-            type: Boolean,
-            default: false
-        },
-        optModRewriteRegexp: {
-            type: String,
-            default: ".*\\.[\\d\\w]+$" // looks like a file extension
-        },
-        optModRewriteNegate: {
-            type: Boolean,
-            default: true
-        },
-        optModRewriteTo: {
-            type: String,
-            default: '/index.html'
         }
-	}
+	};
 
 	function Options(callback) {
-		this.meta = options
-		this.options = null
+		this.meta = options;
+		this.options = null;
 
 		chrome.storage.local.get(null, function(d) {
-			this.options = d
+			this.options = d;
 			// update options with default options
-			callback()
-		}.bind(this))
+			callback();
+		}.bind(this));
 	}
 	Options.prototype = {
 		get: function(k) {
-			if (this.options[k] !== undefined) return this.options[k]
-			return this.meta[k].default
+			if (this.options[k] !== undefined) {
+				return this.options[k];
+			}
+			return this.meta[k].default;
 		},
 		getAll: function() {
-			var d = {}
-			Object.assign(d, this.options)
+			var d = {};
+			Object.assign(d, this.options);
 			for (var key in this.meta) {
 				if (d[key] === undefined && this.meta[key].default !== undefined) {
-					d[key] = this.meta[key].default
+					d[key] = this.meta[key].default;
 				}
 			}
-			return d
+			return d;
 		},
 		set: function(k,v) {
-			this.options[k] = v
-			var d = {}
-			d[k] = v
-			chrome.storage.local.set(d, function(){})
+			this.options[k] = v;
+			var d = {};
+			d[k] = v;
+			chrome.storage.local.set(d, function(){});
 		}
-	}
-	window.AppOptions = Options
+	};
+	window.AppOptions = Options;
 
-})()
+})();

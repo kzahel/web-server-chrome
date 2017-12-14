853c069f523fe16f510f14708f78d7333dbb58fa
Kai Boernert <kai.boernert@oio.de>
2017-12-14 15:22:46 +0100

    adjusted buffers to increase performance

--- stream.js
+++ stream.js
@@ -91,7 +91,7 @@
             }
             //console.log('tryWrite')
             this.writing = true
-            var data = this.writeBuffer.consume_any_max(4096)
+            var data = this.writeBuffer.consume_any_max(4096*1024)
             //console.log(this.sockId,'tcp.send',data.byteLength)
             //console.log(this.sockId,'tcp.send',WSC.ui82str(new Uint8Array(data)))
             sockets.tcp.send( this.sockId, data, this.onWrite.bind(this, callback) )

2465bdfe84883db1772180275760bd945183f278
Kyle Graehl <kyle@graehlarts.com>
2020-01-27 19:15:57 -0800

    add makedeps

---
+++ makedeps/index.js
@@ -0,0 +1,15 @@
+var m
+
+m = require('react')
+window.React = m
+
+m = require('react-dom')
+window.ReactDOM = m
+
+m = require('@material-ui/core')
+window.MaterialUI = m
+
+m = require('underscore')
+window._ = m
+
+module.exports = {}

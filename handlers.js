0802c0b808f82d2065ccc217dbe3d85e778cf52c
Ethan O'Brien <ethan.a.obrien@gmail.com>
2021-07-20 13:47:56 -0500

    bug fix

--- handlers.js
+++ handlers.js
@@ -729,7 +729,7 @@
                                                 var filefound = true
                                             }
                                         }
-                                        if ((origdata[i].request_path == filerequested || origdata[i].request_path == 'all files') && origdata[i].type == 'versioning' && ! filefound && && ! this.request.isVersioning) {
+                                        if ((origdata[i].request_path == filerequested || origdata[i].request_path == 'all files') && origdata[i].type == 'versioning' && ! filefound && ! this.request.isVersioning) {
                                             var data = origdata[i]
                                             var filefound = true
                                         }

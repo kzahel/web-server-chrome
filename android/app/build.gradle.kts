3a80442f9e406a99a5bc47e00293ec74e3e7067c
Kyle Graehl <kgraehl@gmail.com>
2026-08-02 07:36:43 +0200

    Release Android v0.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.ok200.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.0"
+        versionCode = 6
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
